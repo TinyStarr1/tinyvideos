ae518b3d39141f40020b32497c722de80a0802cc
TinyStarr1 <razvanstelian2019@gmail.com>
2026-04-26 20:54:52 +0300

    sd

--- date_clipuri.js
+++ date_clipuri.js
@@ -4,10 +4,10 @@
 const videosData = [
     {
         // Acesta este un video de test (Big Buck Bunny) luat de pe internet ca să vezi cum arată
-        link: "https://www.w3schools.com/html/mov_bbb.mp4",
-        titlu: "Video de Test - Natura",
+        link: "https://rule34video.com/get_file/51/2853df891c0d26ac77494226fedac42d/4333000/4333514/4333514_720p.mp4/?v-acctoken=MjI3MnwzMTE4MTB8MHwyNWNlOWZmY2M0OGZkNmY0MmE0NWY4M2I3MWRlNTI2MAb0ecb72cb228b24a&rnd=1777225918928",
+        titlu: "Melfi Paizuri [Neoreptil]",
         data: "2026-04-26",
-        taguri: ["test", "natura", "demo"],
+        taguri: ["paizuri", "cum", "2d"],
         autor: "admin"
     },
     // Pentru a adăuga un clip nou, șterge barele (/* ... */) de mai jos și completează datele:
