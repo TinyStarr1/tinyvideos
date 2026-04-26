ab0eb90bc1d982646d106cb1a2f7cd1a6b33907f
TinyStarr1 <razvanstelian2019@gmail.com>
2026-04-26 21:15:52 +0300

    Update date_clipuri.js

--- date_clipuri.js
+++ date_clipuri.js
@@ -10,6 +10,14 @@ const videosData = [
         taguri: ["paizuri", "cum", "2d"],
         autor: "admin"
     },
+    {
+        // Acesta este un video de test (Big Buck Bunny) luat de pe internet ca să vezi cum arată
+        link: "https://eu-cdn02-prem.boomio-cdn.com/remote_control.php?file=g6CzbRalJjm1hh2178cbY4vKYh46hsOwQYoUMa-K3TJsjPuaZpjglWM5kU4db4jy7jP5TkffOpxq0N33HNOTP3ZVgXmhlBGOScY0weAOjXCuxi03WHpzwPTCBE3gg8QwMHSh89V2GPzlUO1XBJSJdfUMnDA_mmmwWJpg6BOUd7HGHHXrnRBsc5q4_7yTg2PPi4PfswlrrI4fokt8d0UQPWs7y9Q2MM0.mp4&acctoken=MTBhNzhhNjkzODE0MTQxYWZkMDhhMWRhYzM2NzAxZDM4YjI5M2M3MDhmODg2NGFiNzllMzk4N2VkMjIzOGRkMHwxNzc3MjM0NDIzfDkyMTAwMHxydWxlMzR2aWRlby5jb218MHx8MjFhYjU0ZWViZTg4YTE5ZTk5MGU3NTZlNmI3NTE5MDg",
+        titlu: "Rio Paizuri",
+        data: "2026-04-26",
+        taguri: ["paizuri", "cum", "2d"],
+        autor: "admin"
+    },
     // Pentru a adăuga un clip nou, șterge barele (/* ... */) de mai jos și completează datele:
     /*
     {
@@ -20,4 +28,6 @@ const videosData = [
         autor: "NumeleTau"
     }
     */
+
+    
 ];
